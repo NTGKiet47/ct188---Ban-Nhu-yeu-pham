b4d05bbcb8d677eb5cc0ef7c6f2fa611e5251061
NTGKiet47 <nguyentrangiakiet47@gmail.com>
2023-12-07 07:26:26 +0700

    them chuc nang cho cac trang khac

--- js/order.js
+++ js/order.js
@@ -6,10 +6,8 @@ let close = document.querySelector(".close");
 iconCart.addEventListener("click", function () {
   if (cart.style.right == "-100%") {
     cart.style.right = "0";
-    container.style.transform = "translateX(-400px)";
   } else {
     cart.style.right = "-100%";
-    container.style.transform = "translateX(0)";
   }
 });
 close.addEventListener("click", function () {
